dd36d5a3b39c1a2b7891e92c35d361fe963b7d54
Antoine Reversat <a.reversat@gmail.com>
2019-02-01 15:07:02 -0500

    Add a blog service

---
+++ src/app/blog.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Post } from './post';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+  private base_url = 'http://localhost:8000/posts';
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.base_url);
+  }
+}
---
+++ src/app/post.ts
@@ -0,0 +1,12 @@
+export class Post {
+  author: string;
+  tags: string[];
+  state: string;
+
+  title: string;
+  content: string;
+
+  created_date: string;
+  modified_date: string;
+  published_date: string;
+}
