7330fa5691c1e16eccae9f2b29d130c49ef33188
Antoine Reversat <a.reversat@gmail.com>
2020-10-22 13:22:40 -0400

    Move to scss

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -6,7 +6,7 @@ import { Post } from '../post/post';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
 
