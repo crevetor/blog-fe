97f816309b0c0b32c8ca3579b500fffe78fd5536
Antoine Reversat <a.reversat@gmail.com>
2020-10-22 13:34:11 -0400

    Use chips for tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips'; 
 import { BlogComponent } from './blog/blog.component';
 import { AboutComponent } from './about/about.component';
 import { PostComponent } from './post/post.component';
@@ -48,7 +49,8 @@ const appRoutes: Routes = [
     MatCardModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [
