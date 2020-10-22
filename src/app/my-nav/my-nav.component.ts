5b48eea9b7e8ae01077fbc0b488a3cc098f718f0
Antoine Reversat <a.reversat@gmail.com>
2020-10-22 13:33:23 -0400

    Implement titlebar service

--- src/app/my-nav/my-nav.component.ts
+++ src/app/my-nav/my-nav.component.ts
@@ -3,18 +3,32 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+import { TitleBarService } from '../titlebar.service';
+
 @Component({
   selector: 'app-my-nav',
   templateUrl: './my-nav.component.html',
   styleUrls: ['./my-nav.component.scss'],
+  providers: [TitleBarService]
 })
 export class MyNavComponent {
+  titleBarTitle: string = '';
+  titleBarSubTitle: string = '';
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private titleBarService: TitleBarService) {
+      titleBarService.title$.subscribe(
+        title => {this.titleBarTitle = title}
+      );
+      titleBarService.subTitle$.subscribe(
+        subtitle => {this.titleBarSubTitle = subtitle}
+      );
+    }
 
 }
