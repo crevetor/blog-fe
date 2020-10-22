f170d6dff0327bb052ce5f82dfb2e327a0cb1f61
Antoine Reversat <a.reversat@gmail.com>
2020-10-22 13:27:23 -0400

    Sync post definition to actual model

--- src/app/post/post.ts
+++ src/app/post/post.ts
@@ -1,9 +1,22 @@
+class User {
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+}
+
+class Author {
+  user: User;
+  photo: string;
+}
 export class Post {
-  author: string;
+  id: number;
+  author: Author;
   tags: string[];
   state: string;
 
   title: string;
+  summary: string;
   content: string;
 
   created_date: Date;
